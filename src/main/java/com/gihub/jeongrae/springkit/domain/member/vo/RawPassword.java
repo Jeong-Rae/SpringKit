868ce7d123ea8163902516fb85d32132ead96f48
JRJR519 <kkwjdfo@gmail.com>
2024-07-02 06:11:06 +0900

    feat: RawPassword에 대한 VO 추가 RawPasswordDeserializer 추가하여, Client 요청에 대한 직관 유지

--- src/main/java/com/gihub/jeongrae/springkit/domain/member/vo/RawPassword.java
+++ src/main/java/com/gihub/jeongrae/springkit/domain/member/vo/RawPassword.java
@@ -1,5 +1,9 @@
 package com.gihub.jeongrae.springkit.domain.member.vo;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.gihub.jeongrae.springkit.domain.member.converter.RawPasswordDeserializer;
+
+@JsonDeserialize(using = RawPasswordDeserializer.class)
 public record RawPassword(
         String password
 ) {
