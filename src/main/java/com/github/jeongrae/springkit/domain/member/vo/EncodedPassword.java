4a742b25d4b6bb9b78b85816f699f3fdcc809bcc
Jeong-Rae <kkwjdfo@gmail.com>
2026-09-03 13:54:31 +0000

    RESET

--- src/main/java/com/github/jeongrae/springkit/domain/member/vo/EncodedPassword.java
+++
@@ -1,6 +0,0 @@
-package com.github.jeongrae.springkit.domain.member.vo;
-
-public record EncodedPassword(
-        String password
-) {
-}
--- src/main/java/com/github/jeongrae/springkit/global/util/SuccessResponse.java
+++
@@ -1,6 +0,0 @@
-package com.github.jeongrae.springkit.global.util;
-
-public record SuccessResponse(
-        Boolean success
-) {
-}
