03ecf58c7908d8d0e4cd59af9d9d35c3124984fb
Jeongrae Kim <102293576+Jeong-Rae@users.noreply.github.com>
2026-09-21 20:29:21 +0900

    Build: 단일 모듈 Kotlin 2.2와 Java 24 적용

--- template/spring-boot-kotlin/build.gradle.kts
+++ template/spring-boot-kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  kotlin("jvm") version "2.3.21"
-  kotlin("plugin.spring") version "2.3.21"
+  kotlin("jvm") version "2.2.21"
+  kotlin("plugin.spring") version "2.2.21"
   id("org.springframework.boot") version "4.1.1"
   id("io.spring.dependency-management") version "1.1.7"
   id("com.diffplug.spotless") version "8.10.2"
@@ -16,7 +16,7 @@ base {
 
 java {
   toolchain {
-    languageVersion = JavaLanguageVersion.of(21)
+    languageVersion = JavaLanguageVersion.of(24)
   }
 }
 
