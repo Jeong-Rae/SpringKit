a0df60cbb582cb67a1914ae2686dc242d0b37eda
Jeongrae Kim <102293576+Jeong-Rae@users.noreply.github.com>
2026-09-21 20:29:26 +0900

    Build: 멀티모듈 Kotlin 2.2 적용

--- template/spring-boot-kotlin-multi-module/build.gradle.kts
+++ template/spring-boot-kotlin-multi-module/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  kotlin("jvm") version "2.3.21" apply false
-  kotlin("plugin.spring") version "2.3.21" apply false
+  kotlin("jvm") version "2.2.21" apply false
+  kotlin("plugin.spring") version "2.2.21" apply false
   id("org.springframework.boot") version "4.1.1" apply false
   id("io.spring.dependency-management") version "1.1.7" apply false
   id("com.epages.restdocs-api-spec") version "0.20.1" apply false
