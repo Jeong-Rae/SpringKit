88fddf557cd3abcd2c8ed96be8ac730ed230baa1
Jeongrae Kim <102293576+Jeong-Rae@users.noreply.github.com>
2026-09-24 12:57:11 +0900

    Feature: optional field의 null sample 지원

--- template/spring-boot-kotlin-multi-module/declarative-rest-docs/src/main/kotlin/__SPRINGKIT_PACKAGE_PATH__/declarativerestdocs/Sample.kt
+++ template/spring-boot-kotlin-multi-module/declarative-rest-docs/src/main/kotlin/__SPRINGKIT_PACKAGE_PATH__/declarativerestdocs/Sample.kt
@@ -5,13 +5,13 @@ import kotlin.reflect.typeOf
 
 /** HTTP 값의 대표 값과 선언된 Kotlin 타입 보존 */
 data class Sample(
-    val value: Any,
+    val value: Any?,
     val type: KType,
 )
 
 /** 구체적인 [T] 타입 정보를 보존하는 [Sample] 생성 */
 @OptIn(ExperimentalStdlibApi::class)
-inline fun <reified T : Any> sampleOf(value: T): Sample =
+inline fun <reified T : Any> sampleOf(value: T?): Sample =
     Sample(
         value = value,
         type = typeOf<T>(),
